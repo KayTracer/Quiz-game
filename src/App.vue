2c5d2746a4492b9e8cb588c53507f11b9635004a
KayTracer <cainaluz@gmail.com>
2025-06-30 19:47:51 -0300

    Adcionei o componente ScoreBoard que é responsavél pelo placar. Terminei o App botando o componente, utilizando o props para o ScoreBoard, usei o computed e adcionei os métodos. Projeto finalizado!(Talvez eu atualize ele colocando um Localstorage futuramente.)

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,113 @@
 <template>
   <div>
-    <h1>
-      Microphones can be used not only to pick up sound, but also to project
-      sound similar to a speaker
-    </h1>
+    <ScoreBoard :winCount="winCount" :loseCount="loseCount" />
 
-    <input type="radio" name="options" value="True" />
-    <label>True</label><br />
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
 
-    <input type="radio" name="options" value="False" />
-    <label>False</label><br />
+      <template v-for="(answer, index) in this.answers" v-bind:key="index">
+        <input
+          :disabled="this.answerSubmitted"
+          type="radio"
+          name="options"
+          :value="answer"
+          v-model="this.chosen_answer"
+        />
+        <label v-html="answer"></label><br />
+      </template>
+      <button
+        v-if="!this.answerSubmitted"
+        @click="submitAnswer()"
+        class="send"
+        type="button"
+      >
+        Send
+      </button>
 
-    <button class="send" type="button">Send</button>
+      <section v-if="this.answerSubmitted" class="result">
+        <h4
+          v-if="this.chosen_answer == this.correctAnswer"
+          v-html="
+            '&#9989; Congratulations, the answer ' +
+            this.correctAnswer +
+            ' is correct!'
+          "
+        ></h4>
+        <h4
+          v-else
+          v-html="
+            '&#10060; I`m sorry, you picked the wrong answer. The correct is ' +
+            this.correctAnswer +
+            '.'
+          "
+        ></h4>
+        <button @click="this.getNewQuestion()" class="send" type="button">
+          Next question
+        </button>
+      </section>
+    </template>
   </div>
 </template>
 
 <script>
+import ScoreBoard from "./components/ScoreBoard.vue";
+
 export default {
   name: "App",
+  data() {
+    return {
+      question: undefined,
+      correctAnswer: undefined,
+      incorrectAnswers: undefined,
+      chosen_answer: undefined,
+      answerSubmitted: false,
+      winCount: 0,
+      loseCount: 0,
+    };
+  },
+  components: { ScoreBoard },
 
+  computed: {
+    answers() {
+      let answers = [...this.incorrectAnswers];
+      answers.splice(
+        Math.round(Math.random() * answers.length),
+        0,
+        this.correctAnswer
+      );
+      return answers;
+    },
+  },
+
+  methods: {
+    submitAnswer() {
+      if (!this.chosen_answer) {
+        alert("Pick one of the options!");
+      } else if (this.chosen_answer == this.correctAnswer) {
+        this.winCount++;
+        this.answerSubmitted = true;
+      } else {
+        this.loseCount++;
+        this.answerSubmitted = true;
+      }
+    },
+    getNewQuestion() {
+      this.chosen_answer = undefined;
+      this.answerSubmitted = false;
+      this.question = undefined;
+
+      this.axios
+        .get("https://opentdb.com/api.php?amount=1&category=15")
+        .then((response) => {
+          this.question = response.data.results[0].question;
+          this.correctAnswer = response.data.results[0].correct_answer;
+          this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        });
+    },
+  },
+  created() {
+    this.getNewQuestion();
+  },
   //https://opentdb.com/api.php?amount=1&category=15
 };
 </script>
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="score">
+    Player <span>{{ this.winCount }}</span> x
+    <span>{{ this.loseCount }}</span> Computer
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ScoreBoard",
+  props: ["winCount", "loseCount"],
+};
+</script>
+
+<style lang="scss">
+.score {
+  border-bottom: 1px solid black;
+  padding: 24px;
+  font-size: 20px;
+}
+span {
+  font-weight: bold;
+  border: 1px solid black;
+  padding: 8px;
+}
+</style>
